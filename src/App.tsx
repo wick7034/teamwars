d1f0675c31311db2334cd50515e6fbe8e31efd28
arpan <95864927+wick7034@users.noreply.github.com>
2025-07-13 12:59:02 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useGameState } from './hooks/useGameState';
 import { AuthModal } from './components/AuthModal';
 import { GameBoard } from './components/GameBoard';
